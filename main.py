8d7401083ab9e683de7df9d3646cb076374176df
Davide <davide.vavassori@gmail.com>
2024-01-11 22:07:50 +0100

    added main two columns

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Davide")
+    content ="""
+    Ciao, sono Davide, sto seguendo un corso di python e sono molto appassionato di 
+    programmazione, non veo l'ora di scoprire cosa altro potrò creare!
+    """
+    st.info(content)
