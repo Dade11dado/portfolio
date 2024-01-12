8ae56bdb196ad08af52ec2ae9df73d4d29da648d
Davide <davide.vavassori@gmail.com>
2024-01-12 21:18:04 +0100

    render title and description in 2 columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -18,3 +19,17 @@ with col2:
 content2 = """Qui sotto trovi tutte le applicazioni che ho creato, sentiti pure libero di 
 conttattarmi dalla sezione dedicata!"""
 st.write(content2)
+
+col3,col4 = st.columns(2)
+
+df = pd.read_csv("data.csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
