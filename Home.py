1d9531f3aead5c45ccc0e4ee104284044af23791
Davide <davide.vavassori@gmail.com>
2024-01-13 13:20:47 +0100

    Add navigation menu and contact page

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", )
 
 col1,col2 = st.columns(2)
 
@@ -28,8 +28,12 @@ with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
